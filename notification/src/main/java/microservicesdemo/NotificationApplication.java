0849916b939daab90aece7c52b8e2f0cd6169138
Noureddine El Fatimi <noureddineelfatimi03@gmail.com>
2025-11-07 15:18:31 +0100

    push

--- notification/src/main/java/microservicesdemo/NotificationApplication.java
+++ notification/src/main/java/microservicesdemo/NotificationApplication.java
@@ -5,29 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(
-        scanBasePackages = {
-                "microservicesdemo.notification",
-                "microservicesdemo.amqp",
-        }
-)
+@SpringBootApplication
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner commandLineRunner(
-//            RabbitMQMessageProducer producer,
-//            NotificationConfig notificationConfig
-//            ) {
-//        return args -> {
-//            producer.publish(
-//                    new Person("Ali", 18),
-//                    notificationConfig.getInternalExchange(),
-//                    notificationConfig.getInternalNotificationRoutingKey());
-//        };
-//    }
-//
-//    record Person(String name, int age){}
 }
